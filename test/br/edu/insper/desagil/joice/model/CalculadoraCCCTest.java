8f4fcb41f8ecf750a2ac9e0bc3ddb78a2ca5d66e
Gabriel Couto <38431521+gabrielcla@users.noreply.github.com>
2021-04-05 13:39:40 -0300

    Tests Final

--- test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
@@ -1,11 +1,7 @@
 package br.edu.insper.desagil.joice.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import br.edu.insper.desagil.joice.model.Calculadora;
-import br.edu.insper.desagil.joice.model.CalculadoraCCC;
 
 public class CalculadoraCCCTest {
 
@@ -13,19 +9,19 @@ public class CalculadoraCCCTest {
 
 	@Test
 	public void testNiobio() {
-		calculadora = new CalculadoraCCC("Niobio");
+		calculadora = new CalculadoraCCC();
 		assertEquals(8.57, calculadora.calcula(92.91, 0.143), 0.05);
 	}
 	
 	@Test
 	public void testTungstenio() {
-		calculadora = new CalculadoraCCC("Tungstenio");
+		calculadora = new CalculadoraCCC();
 		assertEquals(19.28, calculadora.calcula(183.84, 0.137), 0.05);
 	}
 	
 	@Test
 	public void testLitio() {
-		calculadora = new CalculadoraCCC("Litio");
+		calculadora = new CalculadoraCCC();
 		assertEquals(0.53, calculadora.calcula(6.94, 0.152), 0.05);
 	}
 }
--- test/br/edu/insper/desagil/joice/model/CalculadoraCFCTest.java
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCFCTest.java
@@ -1,11 +1,7 @@
 package br.edu.insper.desagil.joice.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import br.edu.insper.desagil.joice.model.Calculadora;
-import br.edu.insper.desagil.joice.model.CalculadoraCFC;
 
 public class CalculadoraCFCTest {
 	
@@ -13,19 +9,19 @@ public class CalculadoraCFCTest {
 	
 	@Test
 	public void testCobre() {
-		calculadora = new CalculadoraCFC("Cobre");
+		calculadora = new CalculadoraCFC();
 		assertEquals(8.89, calculadora.calcula(63.55, 0.128), 0.05);
 	}
 
 	@Test
 	public void testOuro() {
-		calculadora = new CalculadoraCFC("Cobre");
+		calculadora = new CalculadoraCFC();
 		assertEquals(19.36, calculadora.calcula(196.97, 0.144), 0.05);
 	}
 	
 	@Test
 	public void testPrata() {
-		calculadora = new CalculadoraCFC("Cobre");
+		calculadora = new CalculadoraCFC();
 		assertEquals(10.60, calculadora.calcula(107.87, 0.144), 0.05);
 	}
 	
