fe853f2992abb3224dbc7e3dc6c1589c5b95d755
Gabriel Couto <38431521+gabrielcla@users.noreply.github.com>
2021-04-05 14:22:49 -0300

    Classes

--- src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -1,5 +1,20 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCCC {
+public class CalculadoraCCC extends Calculadora {
+
+	public CalculadoraCCC() {
+		super("Cúbica de Corpo Centrado");
+	}
+
+	@Override
+	public double calcula(double peso, double raio) {
+		double avogadro = 6.022*Math.pow(10, 23);
+		double massa = 2 *peso/avogadro;
+		
+		double aresta = 4*raio/Math.pow(10, 7) /Math.pow(3, 0.5); 
+		double volume = Math.pow(aresta, 3);
+		
+		return massa/volume;
+	}
 
 }
--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -1,5 +1,20 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCFC {
+public class CalculadoraCFC extends Calculadora{
+
+	public CalculadoraCFC() {
+		super("Cúbica de Face Centrada");
+	}
+
+	@Override
+	public double calcula(double peso, double raio) {
+		double avogadro = 6.022*Math.pow(10, 23);
+		double massa = 4 *peso/avogadro;
+		
+		double aresta = 2*Math.pow(2, 0.5) * raio /Math.pow(10, 7); 
+		double volume = Math.pow(aresta, 3);
+		
+		return massa/volume;
+	}
 
 }
